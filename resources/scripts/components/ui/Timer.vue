d10d2bde242b33edab595120db597b52d8ab2fc3
helloween141 <helloween141@gmail.com>
2022-06-28 23:39:49 +0300

    Timer fix, styles added

--- resources/scripts/components/ui/Timer.vue
+++ resources/scripts/components/ui/Timer.vue
@@ -12,9 +12,19 @@
         class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline right-0">
       Выключить таймер
     </button>
-    <span class="text-white text-2xl">
-      Время выполнения: {{ hour }} ч. {{ min }} мин.
-    </span>
+    <div v-if="hour || min"
+         class="text-white text-1xl flex items-center"
+    >
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+        <path fill-rule="evenodd"
+              d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z"
+              clip-rule="evenodd"/>
+      </svg>
+      <div class="pl-3">
+        <span v-if="hour > 0">{{ hour }} ч.&nbsp;</span>
+        <span v-if="min > 0">{{ min }} мин.</span>
+      </div>
+    </div>
   </div>
 
 </template>
@@ -53,16 +63,23 @@ export default {
       }
     },
     launchTimer() {
-      this.timer = setInterval(() => {
-        this.min++
-        if (this.min === 60) {
-          this.hour++;
-          this.min = 0
-        }
-        this.currentTime = this.min + (this.hour * 60)
+      const date = new Date();
 
-        this.update()
-      }, 1000)
+      setTimeout( () => {
+        this.timer = setInterval(this.calcTime, 60000);
+        this.calcTime();
+      }, (60 - date.getSeconds()) * 1000);
+
+    },
+    calcTime() {
+      this.min++
+      if (this.min === 60) {
+        this.hour++;
+        this.min = 0
+      }
+      this.currentTime = this.min + (this.hour * 60)
+
+      this.update()
     },
     async update() {
       try {
